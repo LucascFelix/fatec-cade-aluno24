47fec92e9c870ea1c6084fb2e165c2a90185a0d1
Lucas Felix <lucacfelix@gmail.com>
2024-03-19 11:09:56 -0300

    :sparkles: Adicionando um novo recurso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cad-a-aluno';
+  
 }
